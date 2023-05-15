70b58c184ce5e986190d368b0c6be80cc50a645b
FusionSid <siddheshadsv@icloud.com>
2023-05-16 10:16:37 +1200

    feat: add route to create a new user

--- api/src/routes/users.py
+++ api/src/routes/users.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from tortoise.exceptions import IntegrityError
+from tortoise.contrib.pydantic import pydantic_model_creator  # type: ignore
+
+from core import NewUser, User
 
 user_router = APIRouter(
     tags=[
@@ -9,5 +13,26 @@ user_router = APIRouter(
 
 
 @user_router.get("/")
-async def get_user():
+async def get_user(request: Request):
     pass
+
+
+@user_router.post("/")
+async def create_new_user(request: Request, user_data: NewUser):
+    try:
+        user = await User.create(
+            username=user_data.username, auth_key_hash=user_data.auth_key_hash
+        )
+    except IntegrityError:
+        return {
+            "success": False,
+            "detail": "User failed to create! most likey because of a username conflict. ",
+            "tip": "Try using another username or trying again later.",
+        }
+
+    user_pyd = await pydantic_model_creator(User, name="User").from_tortoise_orm(user)
+    return {
+        "success": True,
+        "detail": "User created successfully!",
+        "created_user": user_pyd,
+    }

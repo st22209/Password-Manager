8de8ed19f0933e51cbc93b321e77f6e68cfbbdb5
FusionSid <siddheshadsv@icloud.com>
2023-05-19 12:03:17 +1200

    feat: add endpoint for creating a password

--- api/src/routes/passwords.py
+++ api/src/routes/passwords.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from typing import Optional
+from fastapi import APIRouter, Request
 from tortoise.contrib.pydantic import pydantic_model_creator  # type: ignore
-from core import Password
+
+from core import Password, APIHTTPExceptions
 
 password_router = APIRouter(
     tags=[
@@ -12,5 +14,17 @@ pswd_pyd = pydantic_model_creator(Password, name="User")
 
 
 @password_router.get("/")
-async def get_password():
-    pass
+async def get_user(request: Request, password_id: Optional[str] = None):
+    if password_id is not None:
+        exists = await Password.exists(id=password_id)
+        if not exists:
+            raise APIHTTPExceptions.PASSWORD_NOT_FOUND(password_id)
+
+        password = await Password.get(id=password_id)
+        pyd = await pswd_pyd.from_tortoise_orm(password)
+
+        return {"success": True, "password": pyd}
+
+    passwords = await Password.all()
+    pyd = [await pswd_pyd.from_tortoise_orm(pswd) for pswd in passwords]
+    return {"success": True, "passwords": list(pyd)}

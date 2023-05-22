62ded0c58789a9ff5e6c720d4a74c433697b0fa3
FusionSid <siddheshadsv@icloud.com>
2023-05-22 20:48:11 +1200

    feat: callback function for when a user submits

--- app/src/core/formCallbacks.ts
+++ app/src/core/formCallbacks.ts
@@ -1,26 +1,38 @@
 import { bcrypt_hash, postNewUser } from "../core";
 import { Store } from "tauri-plugin-store-api";
 
-async function signupSubmitCallback(
-	event: React.FormEvent<HTMLFormElement>,
-	username: string,
-	password: string
-) {
-	event.preventDefault();
+type User = {
+	success: true;
+	detail: string;
+	user: {
+		id: string;
+		username: string;
+		auth_key_hash: string;
+	};
+};
 
+type NewUserError = {
+	success: false;
+	type: string;
+};
+
+async function signupSubmitCallback(
+	username: string,
+	password: string,
+): Promise<User | NewUserError> {
 	let vaultKey = await bcrypt_hash(`${password}${username}`);
 	let authKey = await bcrypt_hash(`${vaultKey.hash}${username}`);
 
 	let store = new Store("salts.dat");
-	
+
 	await store.set(`${username}-vault-salt`, vaultKey.salt);
 	await store.set(`${username}-auth-salt`, authKey.salt);
-	
+
 	await store.save()
 
-	await postNewUser(username, authKey.hash)
+	return await postNewUser(username, authKey.hash)
 }
 
 export {
-    signupSubmitCallback
+	signupSubmitCallback
 }

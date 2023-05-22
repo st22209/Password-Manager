f85350eff1e67797b7fc2a7179e0eac5be352c79
FusionSid <siddheshadsv@icloud.com>
2023-05-22 12:10:22 +1200

    feat: store username and password and provide it to callback

--- app/src/components/userform.tsx
+++ app/src/components/userform.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 type UserFormProps = {
 	title: string;
-	submitCallback: (e: React.FormEvent<HTMLFormElement>) => void;
+	submitCallback: (
+		e: React.FormEvent<HTMLFormElement>,
+		username: string,
+		password: string
+	) => void | Promise<void>;
 };
+
 const UserForm = ({ title, submitCallback }: UserFormProps) => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<div>
 			<div className="absolute w-full h-full bg-user-background bg-cover"></div>
@@ -15,16 +24,26 @@ const UserForm = ({ title, submitCallback }: UserFormProps) => {
 			<div className="flex flex-col h-screen justify-center items-center relative z-10">
 				<h1 className="text-bold text-white text-[5rem]">{title}</h1>
 				<div>
-					<form onSubmit={submitCallback}>
+					<form
+						onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+							submitCallback(e, username, password);
+						}}
+					>
 						<input
 							className="bg-transparent w-full py-4 px-1 leading-tight focus:outline-none border-b-2 border-white text-[1.2rem] text-[#999999] mb-3"
 							type="text"
+							value={username}
 							placeholder="Username"
+							onChange={(e) => setUsername(e.target.value)}
+							required
 						></input>
 						<input
 							className="bg-transparent w-full py-4 px-1 leading-tight focus:outline-none border-b-2 border-white text-[1.2rem] text-[#999999] mb-10"
 							type="text"
+							value={password}
 							placeholder="Master Password"
+							onChange={(e) => setPassword(e.target.value)}
+							required
 						></input>
 						<br />
 						<div className="flex flex-col justify-center items-center">

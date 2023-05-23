f93c577167b2df29779cc0a515da57fcb7ef92a4
FusionSid <siddheshadsv@icloud.com>
2023-05-23 14:02:40 +1200

    feat: add function to get users auth and vault keys from their username/assword + support for system storage

--- app/src/core/kdf.ts
+++ app/src/core/kdf.ts
@@ -1,4 +1,5 @@
 import { genSalt, hash } from "bcrypt-ts";
+import { Store } from "tauri-plugin-store-api";
 
 async function bcrypt_hash(input: string, salt: string | null = null): Promise<{ hash: string; salt: string; }> {
     if (salt === null) {
@@ -8,6 +9,32 @@ async function bcrypt_hash(input: string, salt: string | null = null): Promise<{
     return { hash: hashed, salt }
 }
 
+async function getUserKeys(
+    username: string,
+    password: string,
+): Promise<Array<{ hash: string, salt: string }>> {
+    let store = new Store("salts.dat");
+
+    let vaultSalt: string | null = await store.get(`${username}-vault-salt`);
+    let authSalt: string | null = await store.get(`${username}-auth-salt`);
+
+    if (vaultSalt === null || authSalt === null) {
+        let vaultKey = await bcrypt_hash(`${password}${username}`);
+        let authKey = await bcrypt_hash(`${vaultKey.hash}${username}`);
+
+        await store.set(`${username}-vault-salt`, vaultKey.salt);
+        await store.set(`${username}-auth-salt`, authKey.salt);
+
+        vaultSalt = vaultKey.salt
+        authSalt = authKey.salt
+    }
+
+    let vaultKey = await bcrypt_hash(`${password}${username}`, vaultSalt);
+    let authKey = await bcrypt_hash(`${vaultKey.hash}${username}`, authSalt);
+
+    return [vaultKey, authKey]
+}
+
 export {
-    bcrypt_hash
+    bcrypt_hash, getUserKeys
 }

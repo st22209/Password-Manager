b9247b750bf9b759226462814149ead250bb2086
FusionSid <siddheshadsv@icloud.com>
2023-05-27 18:49:58 +1200

    feat: create rust handler to save file to disk

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,15 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::fs;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn save_file(path: String, contents: String) {
+    fs::write(path, contents).unwrap();
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![save_file])
         .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
